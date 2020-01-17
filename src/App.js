b161578745c542130af8ecc683772a74884c300b
HEYAN123 <490817982@qq.com>
2020-01-17 21:23:22 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
-import { Button } from "../library";
-// import Scene from './components/Scene';
-// import Tools from './components/Tools';
 
 function App() {
   return (
     <div>
-      <Button>提交</Button>
+      hey,world!
     </div>
   );
 }
