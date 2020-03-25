62fb4981deaf0b08ff99ec092ad01df05463d8eb
HEYAN123 <490817982@qq.com>
2020-03-25 21:32:14 +0800

    🔧 搭建网页环境

---
+++ site/theme/template/Content/MainContent.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "../../../../library";
+
+export default function MainContent() {
+  return (
+    <div className="main-wrapper">
+      <Button>测试</Button>
+    </div>
+  );
+}
