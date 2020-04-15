192b640f2116a04629adcef196caf6bb4c617981
HEYAN123 <490817982@qq.com>
2020-04-15 19:30:58 +0800

    更新url

--- site/theme/template/Home/Banner/Logo.tsx
+++ site/theme/template/Home/Banner/Logo.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
-import LogoPic from './LogoPic';
 import './Logo.less';
 
 export default function Logo() {
   return (
-    <div
-      className="home-card-logo"
-    >
-      <LogoPic />
+    <div className="home-card-logo">
+      <img alt="logo" src="https://hey-design.oss-cn-beijing.aliyuncs.com/logo.png" />
     </div>
   );
 }
